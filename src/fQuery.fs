ebf2caf1146eb5db4f39c0bc400f8ea83a70d23e
Josh <josh.dredge@outlook.com>
2021-11-13 16:31:57 +1000

    Implemented initial functions

--- src/fQuery.fs
+++ src/fQuery.fs
@@ -1,4 +1,130 @@
-module FableQuery
+module fQuery
 
+open System
+open Browser.Types
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable
 open Browser.Dom
+open Browser.Css
+open Browser.CssExtensions
+open Browser.Blob
+open Browser.Event
+open Browser.DomExtensions
 
+
+let console = console
+let array list = JS.Constructors.Array.from list
+
+type fQuery =
+    | Elements of HTMLElement[]
+    | Document of Document
+
+type selector =
+    | String of string
+    | Element of Element
+    | Doc of Document
+
+let f (selector: selector) : fQuery =
+    match selector with
+    | String s -> document.querySelectorAll s
+                    |> JS.Constructors.Array.from
+                    |> Elements
+    | Element element ->
+        match element with
+            | null -> array [] |> Elements
+            | _ ->
+                element.setAttribute ("fquery", "include")
+                let elementList = document.querySelectorAll "[fquery=include]"
+                element.removeAttribute "fquery"
+                elementList
+                    |> JS.Constructors.Array.from
+                    |> Elements
+    | Doc document -> Document document
+
+
+let css (property: string) (value: string) fquery =
+    match fquery with
+    | Elements elements ->
+        elements |> Array.iter (fun elem -> elem.style.setProperty(property, value))
+        fquery
+    | Document _ -> fquery
+
+
+let attr (attribute: string) (value: string) fquery =
+    match fquery with
+    | Elements elements ->
+        elements |> Array.iter (fun elem -> elem.setAttribute(attribute, value))
+        fquery
+    | Document _ -> fquery
+
+let getEventStringAlias event =
+    match event with
+    | "ready" -> "DOMContentLoaded"
+    | _ -> event
+
+
+let eventOnTarget (e: Event) (selector: string) (callback: Event->unit) =
+    let target: HTMLElement = e.target :?> HTMLElement
+    if(target.matches(selector)) then
+        callback e
+
+let on (event: string) (selector: string) (callback: Event -> unit) fquery =
+    let eventString = getEventStringAlias event
+    match fquery with
+    | Elements elements ->
+        elements |> Array.iter (fun elem ->
+            if selector = "" then
+                elem.addEventListener(eventString, callback)
+            else
+                elem.addEventListener(eventString,  fun e -> eventOnTarget e selector callback )
+        )
+    | Document doc ->
+        if selector = "" then
+            doc.addEventListener(eventString, callback)
+        else
+            doc.addEventListener(eventString,  fun e -> eventOnTarget e selector callback )
+
+    fquery
+
+(* Class Functions *)
+
+let addClass (className: string) fquery =
+    match fquery with
+        | Elements elements ->
+            elements
+                |> Array.iter (fun elem -> elem.classList.add className)
+            fquery
+        | _ -> fquery
+
+let removeClass (className: string) fquery =
+    match fquery with
+        | Elements elements ->
+            elements
+                |> Array.iter (fun elem -> elem.classList.remove className)
+            fquery
+        | _ -> fquery
+
+let toggleClass (className: string) fquery =
+    match fquery with
+        | Elements elements ->
+            elements
+                |> Array.iter (fun elem -> elem.classList.toggle className |> ignore)
+            fquery
+        | _ -> fquery
+
+let first fquery =
+    match fquery with
+        | Elements elements ->
+            [elements.[0]]
+                |> array
+                |> Elements
+        | _ -> fquery
+
+let last fquery =
+    match fquery with
+        | Elements elements ->
+            [elements.[elements.Length - 1]]
+                |> array
+                |> Elements
+        | _ -> fquery
---
+++ src/sample.fs
@@ -0,0 +1,24 @@
+﻿module sample
+open fQuery
+open Browser
+open Browser.Types
+
+let docReady (_) =
+
+    let p = f(String "p")
+                |> last
+                |> css "color" "blue"
+                |> css "width" "200px"
+                |> css "height" "200px"
+                |> attr "id" "boob"
+                |> on "click" "" (fun e -> console.log "Hi" )
+                |> addClass "booby"
+                |> removeClass "my-button"
+
+    console.log p
+
+
+f(Doc document)
+    |> on "ready" "" docReady
+    |> on "click" "button" (fun _ -> console.log("Lol"))
+    |> ignore
