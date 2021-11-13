75b3a37fc91ed1bb37e52077fac9119762a666b9
Josh <josh.dredge@outlook.com>
2021-11-13 20:56:00 +1000

    Removed special case for Document. Not 100% sure on if this will be sustainable long term. But for now, if a user passes through f(document) then it translates to f('html') with some special cases for document exclusive events.

--- src/fQuery.fs
+++ src/fQuery.fs
@@ -10,61 +10,53 @@ let inline (~%) (x: ^A) : ^B = (^B : (static member From: ^A -> ^B) x)
 
 let array list = JS.Constructors.Array.from list
 
-type fQuery =
-    | Elements of HTMLElement[]
+type fQuery = HTMLElement[]
 
 type selector =
     | String of string
     | Element of Element
-    | D of Document
+    | Document of Document
     static member inline From(e: Element) = Element e
     static member inline From(s: string) = String s
-    static member inline From(d: Document) = D d
+    static member inline From(d: Document) = Document d
 
 let rec f (selector: selector) : fQuery =
     match selector with
     | String s -> document.querySelectorAll s
                     |> JS.Constructors.Array.from
-                    |> Elements
     | Element element ->
         match element with
-            | null -> array [] |> Elements
+            | null -> array []
             | _ ->
                 element.setAttribute ("fquery", "include")
                 let elementList = document.querySelectorAll "[fquery=include]"
                 element.removeAttribute "fquery"
-                elementList
-                    |> JS.Constructors.Array.from
-                    |> Elements
-    | D _ ->
+                JS.Constructors.Array.from elementList
+    | Document _ ->
             f(%"html")
 
-let private applyUnitFunction (elementFunc: HTMLElement -> unit) fquery =
-    match fquery with
-        | Elements elements ->
-            elements
-                |> Array.iter elementFunc
+let private applyFunctionToAllElements (elementFunc: HTMLElement -> unit) (fquery: fQuery) =
+    fquery
+        |> Array.iter elementFunc
     fquery
 
-let get fquery=
-    match fquery with
-        | Elements e -> e
+let get (fquery: fQuery) = fquery
 
 let css (property: string) (value: string) fquery =
     let elementFunc = fun (elem: HTMLElement) -> elem.style.setProperty(property, value)
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
 let attr (attribute: string) (value: string) fquery =
     let elementFunc = fun (elem: HTMLElement) -> elem.setAttribute(attribute, value)
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
 let text (value: string) fquery =
     let elementFunc = fun (element: HTMLElement) -> element.innerText <- value
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
 let html (value: string) fquery =
     let elementFunc = fun (element: HTMLElement) -> element.innerHTML <- value
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
 let private getEventStringAlias event =
     match event with
@@ -97,26 +89,19 @@ let on (event: string) (selector: string) (callback: Event -> unit) fquery =
 
 let addClass (className: string) fquery =
     let elementFunc = fun (elem: HTMLElement) -> elem.classList.add className
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
 let removeClass (className: string) fquery =
     let elementFunc = fun (elem: HTMLElement) -> elem.classList.remove className
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
 let toggleClass (className: string) fquery =
     let elementFunc = fun (elem: HTMLElement) -> elem.classList.toggle className |> ignore
-    applyUnitFunction elementFunc fquery
+    applyFunctionToAllElements elementFunc fquery
 
-let first fquery =
-    match fquery with
-        | Elements elements ->
-            [elements.[0]]
-                |> array
-                |> Elements
+let first (fquery: fQuery) =
+    [fquery.[0]] |> array
 
-let last fquery =
-    match fquery with
-        | Elements elements ->
-            [elements.[elements.Length - 1]]
-                |> array
-                |> Elements
+let last (fquery: fQuery) =
+    [fquery.[fquery.Length - 1]]
+        |> array
